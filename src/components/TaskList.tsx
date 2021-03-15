712109a38dc6df1e779274f1b0d9b92c85b61a74
Jorge Gonzaga <jsgonzaga@uol.com.br>
2021-03-14 21:12:48 -0300

    :tada: Challenge Finalized

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,16 +16,37 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) return;
+
+    const newTaskInput = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(oldState => [...oldState, newTaskInput]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-  }
+    const newMapTasks = tasks.map(task => task.id === id ? {
+      ...task,
+      isComplete: !task.isComplete
+    } : task);
+
+    setTasks(newMapTasks)
+    }
+  
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
-  }
+    const newFilterTasks = tasks.filter(task => task.id !== id)
 
+    setTasks(newFilterTasks)
+  
+    }
+ 
   return (
     <section className="task-list container">
       <header>
